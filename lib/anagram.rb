fb79c546eb8d87b012136f8e99ebeefa9a207e1f
Mohammed Noori <kmanoorib@gmail.com>
2018-06-11 19:36:36 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,6 +9,8 @@ class Anagram
   
   def match(array)
     matched_word = nil
-    #go through the array and select only elements that equal t
+    #go through the array and select only elements that are the same as @word
+    #to see if they are the same, split each word to its characters and order them
+    #if the characters are ordered the same as the instance variable, then it is an anagram
     array.select {|x| x.split("").sort == @word.split("").sort}
 end
